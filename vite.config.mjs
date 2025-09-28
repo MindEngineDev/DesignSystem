bc8dd32815b2ef4e4ba4dbcf5a58d6d5ea044f58
Mind Engine <v.willems@mindengine.nl>
2025-09-28 10:58:08 +0200

    backup before tidy

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,27 +1,4 @@
-// vite.config.mjs
-import { defineConfig } from 'vite';
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { defineConfig } from "vite";
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist'
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './'),
-      '@styles': resolve(__dirname, './styles'),
-      '@utils': resolve(__dirname, './utils'),
-      '@plugins': resolve(__dirname, './plugins'),
-      '@public': resolve(__dirname, './public'),
-      '@assets': resolve(__dirname, './assets')
-    }
-  }
+  server: { port: 5173, open: "/pages/app-shell.html" }
 });
